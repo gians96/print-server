bcf278ec8a4e536a4d02c042abe6c9910211c694
Gianmarcos <gians96@gmail.com>
2023-03-17 08:43:51 -0500

    exception auth, change host mongodb .env.example

--- src/controllers/auth.ctrl.ts
+++ src/controllers/auth.ctrl.ts
@@ -1,17 +1,28 @@
 import { Request, Response } from "express";
 import { registerNewUser, loginUser } from "../services/auth.srv";
+import { handleHttp } from "../utils/error.handle";
 
 const registerCtrl = async ({ body }: Request, res: Response) => {
-  const response = await registerNewUser(body);
-  res.status(200).send(response);
+  try {
+    const response = await registerNewUser(body);
+    res.status(200).send(response);
+  } catch (error) {
+    handleHttp(res, "ERROR_REGISTER_AUTH");
+  }
+
 };
 
 const loginCtrl = async ({ body }: Request, res: Response) => {
-  const { email, password } = body;
-  const response = await loginUser({ email, password });
-  if (response === "PASSWORD_INCORRECT") return res.status(403).send(response);
+  try {
+    const { email, password } = body;
+    const response = await loginUser({ email, password });
+    if (response === "PASSWORD_INCORRECT") return res.status(403).send(response);
 
-  res.status(200).send(response);
+    res.status(200).send(response);
+  } catch (error) {
+    handleHttp(res, "ERROR_LOGIN_AUTH");
+  }
+
 };
 
 export { registerCtrl, loginCtrl };
