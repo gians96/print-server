7e221df258d800c82e715dd0f6d9731c3e39488e
Gians96 <gians96@gmail.com>
2024-02-05 12:05:51 -0500

    [ADD] Se desacoplo la logica del API, con los servicios, se conecto con Firebase

--- src/controllers/print.ctrl.ts
+++ src/controllers/print.ctrl.ts
@@ -1,201 +1,46 @@
 import { Request, Response } from "express";
 import type { Printer } from '../interfaces/PrinterConfig.interface'
-import { Table } from "../interfaces/Table.interface";
-
+import type { Table } from "../interfaces/Table.interface";
+import { isConnectedPrintSrv, printCommandSrv, printPreAccountSrv } from "../services/print.srv";
 const printCommand = async ({ body }: Request, res: Response) => {
     try {
         const configPrinter: Printer = body.configPrinter
         const table: Table = body.table
-        const ThermalPrinter = require("node-thermal-printer").printer;
-        const PrinterTypes = require("node-thermal-printer").types;
-
-        let printer = new ThermalPrinter({
-            type: PrinterTypes.EPSON,
-            interface: 'tcp://' + configPrinter.ip
-        });
-
-        let isConnected = false
-        for (let i = 0; i < 4; i++) {
-            isConnected = await printer.isPrinterConnected();
-            if (isConnected) break
-            await waitExcecute(30);
-        }
-        if (!isConnected) return res.status(400).send({ status: false, msg: "Impresora con ip: " + configPrinter.ip + " no encontrada." })
-
-        printer.alignCenter();
-        printer.setTextDoubleHeight();
-        printer.setTextDoubleWidth();
-        printer.println("COMANDA");
-        printer.println("");
-        printer.tableCustom(
-            [
-                { text: "AMBIENTE:", align: "LEFT", width: 0.25 },
-                { text: table.environment, align: "LEFT", width: 0.25 },
-            ]);
-        printer.tableCustom(
-            [
-                { text: "MOZO:", align: "LEFT", width: 0.25 },
-                { text: table.waiter ? (table.waiter.name ? table.waiter.name : '') : '', align: "LEFT", width: 0.25, bold: true },
-            ]);
-        printer.tableCustom(
-            [
-                { text: "MESA:", align: "LEFT", width: 0.25 },
-                { text: table.label, align: "LEFT", width: 0.25, bold: true },
-            ]);
-        printer.tableCustom(
-            [
-                { text: "PREPARA:", align: "LEFT", width: 0.25 },
-                { text: table.tag.name, align: "LEFT", width: 0.25, bold: true },
-            ]);
-        printer.setTextNormal();
-
-        printer.drawLine();
-        printer.alignLeft();
-        // printer.setTypeFontB();
-
-        // printer.setTextNormal();
-        
-
-        table.products.forEach((product, index) => {
-        printer.setTextSize(1, 1);  //(ANCHO,ALTO)
-
-            // printer.println((index + 1) + ") [" + product.quantity + "] " + product.name);
-            printer.println("[" + product.quantity + "] " + product.name);
-            if (product.note) {
-                printer.println("N: " + product.note);
-            }
-            // printer.newLine();
-            printer.setTextNormal();
-            printer.drawLine();
-
-        });
-        printer.cut();
-        try {
-            let execute = printer.execute()
-            res.status(200).send({ status: true, msg: "impresión exitosa" })
-        } catch (error) {
-            res.status(500).send({ status: false, msg: "Error al imprimir: " + error })
+        let response = await printCommandSrv(configPrinter, table)
+        if (response.status) {
+            res.status(200).send(response)
+        } else {
+            res.status(400).send(response)
         }
     } catch (error) {
         res.status(500).send({ status: false, msg: "Error al imprimir: " + error })
-
     }
 };
 
 const printPreAccount = async ({ body }: Request, res: Response) => {
-
     try {
-
         const configPrinter: Printer = body.configPrinter
         const table: Table = body.table
-        const ThermalPrinter = require("node-thermal-printer").printer;
-        const PrinterTypes = require("node-thermal-printer").types;
-        let printer = new ThermalPrinter({
-            type: PrinterTypes.EPSON,
-            interface: 'tcp://' + configPrinter.ip
-        });
-        let isConnected = false
-        for (let i = 0; i < 4; i++) {
-            isConnected = await printer.isPrinterConnected();
-            if (isConnected) break
-            await waitExcecute(50);
-        }
-        printer.setTextDoubleHeight();
-        printer.setTextDoubleWidth();
-        printer.alignCenter();
-        printer.println("PRECUENTA");
-        printer.println("");
-        printer.setTextNormal();
-        printer.alignLeft();
-
-        printer.tableCustom(
-            [
-                { text: "AMBIENTE:", align: "LEFT", width: 0.25 },
-                { text: table.environment, align: "LEFT", width: 0.25 },
-            ]);
-        printer.tableCustom(
-            [
-                { text: "MOZO:", align: "LEFT", width: 0.25 },
-                { text: table.waiter ? (table.waiter.name ? table.waiter.name : '') : '', align: "LEFT", width: 0.25, bold: true },
-            ]);
-        printer.tableCustom(
-            [
-                { text: "MESA:", align: "LEFT", width: 0.25 },
-                { text: table.label, align: "LEFT", width: 0.25, bold: true },
-            ]);
-        printer.tableCustom(
-            [
-                { text: "FECHA:", align: "LEFT", width: 0.25 },
-                { text: `${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}`, align: "LEFT", width: 0.75, bold: true },
-            ]);
-        if (!isConnected) return res.status(400).send({ status: false, msg: "Impresora con ip: 192.168.18.166 no encontrada." })
-        printer.println("");
-        printer.drawLine();
-        printer.tableCustom([
-            { text: "CANT", align: "LEFT", cols: 5 },
-            { text: "DESCRIPCION", align: "CENTER", cols: 27 },
-            { text: "P. UNI.", align: "CENTER", cols: 8 },
-            { text: "TOTAL", align: "CENTER", cols: 8 },
-        ]);
-
-        printer.drawLine();
-
-
-        table.products.forEach((product, index) => {
-            printer.tableCustom([
-                { text: product.quantity, align: "LEFT", cols: 5, width: 0.11 },
-                { text: product.name, align: "LEFT", cols: 27, width: 0.56 },
-                { text: product.price, align: "LEFT", cols: 8, width: 0.14 },
-                { text: (product.price * product.quantity), align: "LEFT", cols: 8, width: 0.14 },
-            ]);
-            // printer.drawLine();
-        });
-        printer.drawLine();
-        printer.tableCustom([
-            { text: "", align: "LEFT", cols: 7 },
-            { text: "", align: "LEFT", cols: 20 },
-            { text: "TOTAL:", align: "LEFT", cols: 8 },
-            { text: "S/." + table.total, align: "LEFT", cols: 15 },
-        ]);
-        printer.println("DNI/RUC:");
-        printer.println("");
-        printer.println("");
-        printer.println("NOMBRE/RAZON SOCIAL:");
-        printer.println("");
-
-        printer.cut();
-
-        try {
-            let execute = printer.execute()
-            res.status(200).send({ status: true, msg: "impresión exitosa" })
-        } catch (error) {
-            res.status(500).send({ status: false, msg: "Error al imprimir: " + error })
+        let response = await printPreAccountSrv(configPrinter, table)
+        if (response.status) {
+            res.status(200).send(response)
+        } else {
+            res.status(400).send(response)
         }
     } catch (error) {
         res.status(500).send({ status: false, msg: "Error al imprimir: " + error })
-
     }
 };
 
 const isConnectedPrint = async ({ body }: Request, res: Response) => {
     try {
         const configPrinter: Printer = await body.printer
-        const ThermalPrinter = require("node-thermal-printer").printer;
-        const PrinterTypes = require("node-thermal-printer").types;
-        let printer = new ThermalPrinter({
-            type: PrinterTypes.EPSON,
-            interface: 'tcp://' + configPrinter.ip
-        });
-        let isConnected = false
-        for (let i = 0; i < 4; i++) {
-            isConnected = await printer.isPrinterConnected();
-            if (isConnected) break
-            await waitExcecute(30);
+        let response = await isConnectedPrintSrv(configPrinter)
+        if (response.status) {
+            res.status(200).send(response)
+        } else {
+            res.status(400).send(response)
         }
-        if (!isConnected) { return res.status(400).send({ status: false, msg: "Impresora con ip: " + configPrinter.ip + " no encontrada." }) }
-
-        return res.status(200).send({ status: true, msg: "Impresora con ip: " + configPrinter.ip + " conectada." })
-
     } catch (error) {
         return res.status(500).send({ status: false, msg: "Hubo un error al conectar la impresora: " + error })
     }
@@ -204,7 +49,6 @@ const isConnectedPrint = async ({ body }: Request, res: Response) => {
 const isConnectedServer = async ({ body }: Request, res: Response) => {
     try {
         return res.status(200).send({ status: true, msg: "Servidor conectado" })
-
     } catch (error) {
         return res.status(500).send({ status: false, msg: "Servidor no conectado" + error })
     }
@@ -213,18 +57,8 @@ const isConnectedServer = async ({ body }: Request, res: Response) => {
 function waitExcecute(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-
-
-
-
-// 
-
-
 const printAccountTest = async ({ body }: Request, res: Response) => {
-
     try {
-
         const configPrinter: Printer = body.configPrinter
         const table: Table = body.table
         const ThermalPrinter = require("node-thermal-printer").printer;
